16f7575c61cc2ca5a36def349ac99f38c4819978
Vera <lizeth.veraro@outlook.com>
2020-05-25 15:25:39 -0700

    reverse_in_place_code

--- reverse_words_inplace.rb
+++ reverse_words_inplace.rb
@@ -1,13 +1,11 @@
 message = "cake pound steal"
-# When writing your method, assume the message contains only 
+# When writing your method, assume the message contains only
 # letters and spaces, and all words are separated by one space.
 # Prints: 'steal pound cake'
 def reverse_string(string, first, last)
-  # first = 0
-  # last = string.length - 1
 
-  while first < last && string.length 
-    string[first], string[last] = string[last],string[first]
+  while first < last && string.length
+    string[first], string[last] = string[last], string[first]
     first += 1
     last -= 1
   end
@@ -15,18 +13,16 @@ def reverse_string(string, first, last)
 end
 
 def reverse_words(message)
-  first = 0 
-  last = message.length - 1
-  reverse = reverse_string(message, first, last)
-  
-  reverse.each_char.with_index do |word, i|
-    if word == " "
-      reverse_string(word,first,i )
-      p i + 1
-      first = i + 1
-    end
+  reverse_string(message, 0, message.length - 1)
+  current_index = 0
+
+  (0..message.length).each do |i|
+    next unless i == message.length || message[i] == " "
+    reverse_string(message, current_index, i - 1)
+    current_index = i + 1
   end
+  return message
 end
 
-# p reverse_string(message)
-p reverse_words(message)
+
+reverse_words(message)
